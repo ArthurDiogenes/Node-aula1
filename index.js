4307facbed659b9baf28f0055296a24812bcee62
Arthur Diógenes <128557042+ArthurDiogenes@users.noreply.github.com>
2025-05-16 15:08:25 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/Home', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/About', (req, res) => {
+    res.send('email: emailchave@email.com');
+});
+
+app.get('/Features', (req, res) => {
+    res.send('Brabo do PDD');
+});
+
+app.get('/Contact', (req, res) => {
+    res.send('Meu número de telefone é: 11999999999');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
